e8a6e094b295a5b102db5ee10ee920ad7bfdba01
Anderson Alves <andersonalves.me@gmail.com>
2021-11-08 11:02:40 -0300

    Adjusted comments.

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,8 +7,8 @@ const CONST_ZERO = 0;
 
 /**
  * Rest request example
- * - local: http://localhost:8080/
- * - server: https://??/
+ * - local: http://localhost:8080/api
+ * - server: https://shawandpartnersnode.herokuapp.com/api
  **/
 router.get('/api', (req, res, next) => {
     res.status(200).send({
@@ -20,7 +20,7 @@ router.get('/api', (req, res, next) => {
 /**
  * Rest request example
  * - local: http://localhost:8080/api/users?per_page=3&since=1500
- * - server: https://shawandpartnersspring.herokuapp.com/api/users?per_page=3&since=1500
+ * - server: https://shawandpartnersnode.herokuapp.com/api/users?per_page=3&since=1500
  **/
 router.get('/api/users', (req, res, next) => {
     const perPage = req.query.per_page || CONST_FIVE;
@@ -32,7 +32,7 @@ router.get('/api/users', (req, res, next) => {
 /**
  * Rest request example
  * - local: http://localhost:8080/api/users/andersonalvesme/details
- * - server: https://shawandpartnersspring.herokuapp.com/api/users/andersonalvesme/details
+ * - server: https://shawandpartnersnode.herokuapp.com/api/users/andersonalvesme/details
  **/
 router.get('/api/users/:username/details', (req, res, next) => {
     const username = req.params.username;
@@ -43,7 +43,7 @@ router.get('/api/users/:username/details', (req, res, next) => {
 /**
  * Rest request example
  * - local: http://localhost:8080/api/users/brunobertolini/repos?per_page=3&page=2
- * - server: https://shawandpartnersspring.herokuapp.com/api/users/brunobertolini/repos?per_page=3&page=2
+ * - server: https://shawandpartnersnode.herokuapp.com/api/users/brunobertolini/repos?per_page=3&page=2
  **/
 router.get('/api/users/:username/repos', (req, res, next) => {
     const username = req.params.username;
